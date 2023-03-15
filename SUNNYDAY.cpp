05547a680c2923d7977dd39a5213748109e214f4
AmritGaur <84451762+AmritGaur@users.noreply.github.com>
2023-03-15 20:47:39 +0530

    Add files via upload

---
+++ SUNNYDAY.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int X;
+	    cin>>X;
+	    if(X>24){
+	        cout<<"yes\n";
+	    }
+	    else{
+	        cout<<"no\n";
+	    }
+	}
+	return 0;
+}
