9ff96e511d487da2fbf7e3ea129339012400e5e3
Amrit Gaur <84451762+AmritGaur@users.noreply.github.com>
2023-04-16 02:01:18 +0530

    Time: 70 ms (70.24%), Space: 21.9 MB (37.44%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -4,7 +4,7 @@ public:
         queue<pair<int,pair<int,int>>>q;
         int n= grid.size();
         vector<vector<int>>dis(n,vector<int>(n,INT_MAX));
-        vector<vector<bool>>vis(n,vector<bool>(n,false));
+        //vector<vector<bool>>vis(n,vector<bool>(n,false));
         if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
@@ -12,7 +12,7 @@ public:
             return 1;
         }
         dis[0][0]=0;
-        vis[0][0]=true;
+       // vis[0][0]=true;
         q.push({0,{0,0}});
         int dr[]={0,0,1,1,1,-1,-1,-1};
         int dc[]={-1,1,0,1,-1,0,1,-1}; 
@@ -26,13 +26,13 @@ public:
             for(int i=0;i<8;i++){
                 int row=r+dr[i];
                 int col=c+dc[i];
-                if(row>=0 && col>=0 && row<n && col<n && grid[row][col]==0 && dis[row][col]>d+1 && !vis[row][col]){
+                if(row>=0 && col>=0 && row<n && col<n && grid[row][col]==0 && dis[row][col]>d+1){
                     dis[row][col]=d+1;
                     if(row==n-1 && col==n-1){
                            return d+2;
                      }
                     q.push({d+1,{row,col}});
-                    vis[row][col]=true;
+                    //vis[row][col]=true;
                 }
             }
         }
