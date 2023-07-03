61f8bcacbbf03d3b749ae4736a4dcb56f1de6b74
Amrit Gaur <84451762+AmritGaur@users.noreply.github.com>
2023-07-03 14:56:20 +0530

    Time: 6 ms (44.19%), Space: 6.7 MB (99.57%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if (s.length() != goal.length())
+            return false;
+        
+        if (s == goal) {
+            vector<int> count(26, 0);
+            
+            for (int i = 0; i < s.length(); i++) {
+                int c = s[i] - 'a';
+                count[c]++;
+                if (count[c] == 2)
+                    return true;
+            }
+            
+            return false;
+        }
+        
+        int index1 = -1;
+        int index2 = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != goal[i]) {
+                if (index1 == -1)
+                    index1 = i;
+                else if (index2 == -1)
+                    index2 = i;
+                else
+                    return false;
+            }
+        }
+        
+        if (index2 == -1)
+            return false;
+        
+        return (s[index1] == goal[index2] && s[index2] == goal[index1]);
+    }
+};
